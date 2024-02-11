e0905d44fb7900a547356bcdd0d0b6830cc7b3c2
Júlio César <juliobento237@gmail.com>
2024-02-11 18:35:34 -0300

    (feat): initial header and footer component

---
+++ src/components/header.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { MenuIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import SideMenu from "./side-menu";
+import { Button } from "./ui/button";
+import { Card, CardContent } from "./ui/card";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+
+const Header = () => {
+  return (
+    <header>
+      <Card>
+        <CardContent className="p-5 justify-between items-center flex flex-row">
+          <Link href="/">
+            <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <MenuIcon size={16} />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent className="p-0">
+              <SideMenu />
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
+  );
+};
+
+export default Header;
